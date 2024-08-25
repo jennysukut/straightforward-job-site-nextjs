0724ee344b86016b6301f09ef1a087cd49b5bede
JennySukut <jennysukut@gmail.com>
2024-08-25 20:34:33 +0200

    Added Color Scheme + Initial Button Utility Class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center align-middle justify-between p-24 bg-pink-200">
-      <h1 className="text-green-500">Straightforward Job Site</h1>
+    <main className="flex min-h-screen flex-col items-center align-middle p-24 bg-cream">
+      <h1 className="text-jade">Straightforward Job Site</h1>
+      <p className="text-lilac italic">smaller text</p>
+      <div className="button bg-jade drop-shadow-limeShadow">test button</div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,43 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        //If you need a reference, there are color names outlined on the Figma design with the Color Palette
+        // cream + purple
+        cream: "#F8F2EC",
+        lilac: "#D6A6DC",
+        // teal + green shades
+        sky: "#C1D8D3",
+        jade: "#50B09F",
+        emerald: "#308B7B",
+        midnight: "#0C776D",
+        lime: "#D5CB2C",
+        olive: "#A6A646",
+        // pinks + peaches + oranges 
+        // {apricot is more orange-hued than the peach shade}
+        blush: "#F8DAD0",
+        peach: "#FFA08B",
+        apricot: "#FDA26B",
+        orange: "#F38143",
+        watermelon: "#FFABDA",
+        magenta: "#E673A9"
+      }
     },
+    dropShadow: {
+      'lilacShadow': '7px 7px 0px #D6A6DC',
+      'skyShadow': '7px 7px 0px #C1D8D3',
+      'jadeShadow': '7px 7px 0px #50B09F',
+      'emeraldShadow': '7px 7px 0px #308B7B',
+      'midnightShadow': '7px 7px 0px #0C776D',
+      'limeShadow': '7px 7px 0px #D5CB2C',
+      'oliveShadow': '7px 7px 0px #A6A646',
+      'blushShadow': '7px 7px 0px #F8DAD0',
+      'peachShadow': '7px 7px 0px #FFA08B',
+      'apricotShadow': '7px 7px 0px #FDA26B',
+      'orangeShadow': '7px 7px 0px #F38143',
+      'watermelonShadow': '7px 7px 0px #FFABDA',
+      'magentaShadow': '7px 7px 0px #E673A9',
+    }
   },
   plugins: [],
 };
