4ad0aeab17771f26f1d52a5e323b43d1d0fdf5a1
JennySukut <jennysukut@gmail.com>
2024-08-25 21:58:53 +0200

    Added Font and Containers to Global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const myFont = localFont({
+  src: './fonts/Satoshi-Bold.woff2',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center align-middle p-24 bg-cream">
-      <h1 className="text-jade">Straightforward Job Site</h1>
-      <p className="text-lilac italic">smaller text</p>
-      <div className="button bg-jade drop-shadow-limeShadow">test button</div>
+      <h1 className="text-jade text-3xl">Straightforward Job Site</h1>
+      <p className="text-lilac italic tracking-widest">testing out some tailwind classes + options</p>
+      <button className="button bg-jade drop-shadow-smLime">test button</button>
+      <button className="button bg-watermelon drop-shadow-olive max-w-52">new button with more text</button>
+
+      <div className="borderContainer">
+        <h3 className="text-jade text-lg">Here's a Heading</h3>
+        <p className="text-jade">Smaller text test. Let's see what happens when we put more and more info in here.</p>
+      </div>
+
+      <div className="borderContainer">
+        <h3 className="text-jade text-lg">Another Box</h3>
+        <p className="text-jade italic text-sm">Here goes some italic text. Let's try and make it a bit smaller too and see what that does.</p>
+      </div>
+
+      <div className="solidContainer bg-emerald drop-shadow-apricot">
+        <h3 className="text-lg">Testing header</h3>
+        <p className="text-sm">Smaller text going here - it should be cream?</p>
+      </div>
     </main>
   );
 }
