0670819fc0930fc77a4e5fbdb393aa66f3160d89
JennySukut <jennysukut@gmail.com>
2024-08-26 15:42:10 +0200

    Testing Change State with Button

--- src/app/button.tsx
+++ src/app/button.tsx
@@ -3,14 +3,15 @@
 import React, { ChangeEvent, useState, MouseEvent } from "react";
 
 export default function TestButton() {
+    const [clickedButton, setClickedButton] = useState("");
+
 // Apparently you have to make a client component to be able to add event listeners / pass functions into an element
-//Since this is now a component, we should pass in the relevant text / colors / function to be activated on Click? 
-//Alternately, we can just make special individual buttons as needed here and drop them into the necessary page
+// Since this is now a component, we should pass in the relevant text / colors / function to be activated on Click? 
+// Alternately, we can just make special individual buttons as needed here and drop them into the necessary page?
 
 const clickTest = (event : React.MouseEvent<HTMLButtonElement>) => {
     console.log("clicked button test");
-    console.log(event.target);
-    //I keep getting errors when trying to access the classlist to change the button on click
+    setClickedButton("clickMeButton")
 }
-  return <button className="button bg-orange drop-shadow-smSky" onClick={clickTest}>click me</button>;
+  return <button className={clickedButton === "clickMeButton" ? "button bg-lilac drop-shadow-smJade" : "button bg-orange drop-shadow-smSky"} onClick={clickTest}>click me</button>;
 }
