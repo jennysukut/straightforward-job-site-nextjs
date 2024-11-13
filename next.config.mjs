a629fec80399cb21677c8a54f7abb72311c21c17
Carl Thronson <carlthronson@gmail.com>
2024-11-13 14:27:18 -0800

    API_KEY and API_URL.  Put these in .env.local
    API_KEY=whatever
    API_URL=http://localhost:8082/graphql
    
    When it runs on Vercel, these values will be set in the environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    RESEND_API_KEY: "re_ZacMr3bG_MZCB3tg788UrUieq3XgMh3hX",
+    API_KEY: process.env.API_KEY,
+    //when the server is run locally, I believe this key is "whatever"
+    API_URL: process.env.API_URL,
+  },
+};
 
 export default nextConfig;
