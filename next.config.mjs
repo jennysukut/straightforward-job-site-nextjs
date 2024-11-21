19c5cdcf10de117b5749702902c201e4cc7b5e8d
Lean <leian1306@gmail.com>
2024-11-21 09:57:26 +0100

    Remove env variables from config and add example env file
    This means each person will have to duplicate this .env.example and rename it to .env.local with appropriate values

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    RESEND_API_KEY: "re_ZacMr3bG_MZCB3tg788UrUieq3XgMh3hX",
-    API_KEY: process.env.API_KEY,
-    //when the server is run locally, I believe this key is "whatever"
-    API_URL: process.env.API_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
