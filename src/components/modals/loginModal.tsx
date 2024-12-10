b40c117c1a0be0f2ba3eb546945d8ed67886de80
JennySukut <jennysukut@gmail.com>
2024-12-10 13:09:44 -0700

    fixed login modal

--- src/components/modals/loginModal.tsx
+++ src/components/modals/loginModal.tsx
@@ -1,48 +1,85 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import * as z from "zod";
+
 import SiteButton from "../siteButton";
-import { useModal } from "@/contexts/ModalContext";
 import SignupOptionsModal from "./signupModals/signupOptionsModal";
+import FormInputComponent from "../formInputComponent";
+import FormSubmissionButton from "../formSubmissionButton";
+
+import { useState } from "react";
+import { useModal } from "@/contexts/ModalContext";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const LoginSchema = z.object({
+  email: z.string().email({ message: "Email Title Required" }),
+  password: z.string().min(2),
+});
+
+type FormData = z.infer<typeof LoginSchema>;
 
 export default function LoginModal() {
-  const { replaceModalStack } = useModal();
+  const { replaceModalStack, showModal } = useModal();
+  const [disabledButton, setDisabledButton] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setDisabledButton(true);
+    //Login Details Here!
+  };
+
   return (
     <div className="LoginModal flex flex-col items-center gap-10">
-      <h2 className="Title w-full text-center text-2xl font-semibold">login</h2>
-      <form className="LoginForm">
-        <div className="FormContainer flex flex-col gap-10">
-          <div className="FormItem flex flex-col gap-3">
-            <label className="FormLabel font-bold">email</label>
-            <input
-              className="FormInput h-12 w-[460px] border-b-2 border-jade bg-transparent text-jade placeholder:text-jade placeholder:opacity-50"
-              type="email"
-              placeholder="hello@example.com"
-            />
-          </div>
-          <div className="FormItem flex flex-col gap-3">
-            <label className="FormLabel font-bold">password</label>
-            <input
-              className="FormInput h-12 w-[460px] border-b-2 border-jade bg-transparent text-jade placeholder:text-jade placeholder:opacity-50"
-              type="password"
-              placeholder="********"
-            />
-          </div>
-        </div>
-        <div className="FormButtons mt-8 flex justify-between">
+      <Dialog.Title className="Title -mb-2 max-w-[450px] self-center text-center text-xl font-bold">
+        login
+      </Dialog.Title>
+      <form
+        className="LoginForm xs:pt-8 flex min-w-[30vw] flex-col gap-8"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {/* email input */}
+        <FormInputComponent
+          type="email"
+          title="email*"
+          placeholderText="FantasticEmail@email.com"
+          register={register}
+          registerValue="email"
+          errors={errors.email}
+        />
+
+        {/* password input */}
+        <FormInputComponent
+          type="password"
+          title="password*"
+          placeholderText="**********"
+          register={register}
+          registerValue="password"
+          errors={errors.password}
+        />
+
+        {/* form submission buttons */}
+        <div className="LoginButtonsContainer flex justify-between">
           <button
-            className="SignupButton text-sm opacity-80 hover:opacity-100"
-            aria-label="Sign Up"
+            className="SignupOption text-xs text-jade hover:text-darkJade"
             onClick={() => replaceModalStack(<SignupOptionsModal />)}
-            type="button"
           >
-            or sign up
+            or signup
           </button>
-          <SiteButton
-            addClasses="w-32"
-            variant="hollow"
-            aria="Enter"
-            colorScheme="c1"
-          >
-            Enter
-          </SiteButton>
+          <FormSubmissionButton
+            disabledButton={disabledButton}
+            handleSubmit={handleSubmit(onSubmit)}
+            addText="login"
+            addingText="Logging In..."
+          />
         </div>
       </form>
     </div>
