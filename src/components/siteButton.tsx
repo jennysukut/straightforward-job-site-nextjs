c6dc7739c97deb7d84813c6a72a2da4812ea7a9f
JennySukut <jennysukut@gmail.com>
2024-12-11 22:34:44 -0700

    added ability to change colors based on colorContext to SiteButton

--- src/components/siteButton.tsx
+++ src/components/siteButton.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
+import { useColors } from "@/contexts/ColorContext";
+import { useColorOptions } from "@/lib/stylingData/colorOptions";
 import {
   buttonColors,
+  highContrastButtonColors,
   ButtonColorOption,
 } from "@/lib/stylingData/buttonColors";
 import clsx from "clsx";
@@ -38,6 +41,12 @@ const SiteButton: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const [isPressed, setIsPressed] = useState(false);
+  const { colorOption } = useColors();
+  const { textColor } = useColorOptions();
+
+  const hollowOptions =
+    colorOption === "highContrast" ? "border-cobalt " : "border-jade";
+  const bgOptions = colorOption === "highContrast" ? "bg-cobalt" : "bg-jade";
 
   const buttonClasses = clsx(
     "SiteButton relative z-[1] cursor-pointer rounded-full font-semibold transition-all duration-200 tracking-superwide hover:saturate-[120%]",
@@ -53,14 +62,18 @@ const SiteButton: React.FC<ButtonProps> = ({
         size === "extraLarge",
 
       // variant
-      "bg-cream border-jade border-[2px]": variant === "hollow",
-      "text-jade": variant === "hollow" && !isSelected,
+      [`bg-cream ${hollowOptions} border-[2px]`]: variant === "hollow",
+      [`${textColor}`]: variant === "hollow" && !isSelected,
       [`text-eggshell ${buttonColors[colorScheme].color1}`]:
-        variant === "filled",
+        variant === "filled" && colorOption === "standard",
+      [`text-eggshell ${highContrastButtonColors[colorScheme].color1}`]:
+        variant === "filled" && colorOption === "highContrast",
       [`${addImage} bg-cover`]: variant === "avatar",
       // hover colors for hollow buttons
       [`${buttonColors[colorScheme].color5} ${buttonColors[colorScheme].color6} group-hover:text-eggshell`]:
-        variant === "hollow",
+        variant === "hollow" && colorOption === "standard",
+      [`${highContrastButtonColors[colorScheme].color5} ${highContrastButtonColors[colorScheme].color6} group-hover:text-eggshell`]:
+        variant === "hollow" && colorOption === "highContrast",
 
       // pressed state
       "translate-x-2 translate-y-2 sm:translate-x-1.5 sm:translate-y-1.5":
@@ -70,9 +83,13 @@ const SiteButton: React.FC<ButtonProps> = ({
 
       //selected state
       [`${buttonColors[colorScheme].color1} ${buttonColors[colorScheme].color3} translate-x-1 translate-y-1 text-eggshell`]:
-        isSelected,
+        isSelected && colorOption === "standard",
+      [`${highContrastButtonColors[colorScheme].color1} ${highContrastButtonColors[colorScheme].color3} translate-x-1 translate-y-1 text-eggshell`]:
+        isSelected && colorOption === "highContrast",
       [`${buttonColors[colorScheme].color5} ${buttonColors[colorScheme].color6} translate-x-[2px] translate-y-[2px] text-eggshell`]:
-        isSelected && size === "smallCircle",
+        isSelected && size === "smallCircle" && colorOption === "standard",
+      [`${highContrastButtonColors[colorScheme].color5} ${highContrastButtonColors[colorScheme].color6} translate-x-[2px] translate-y-[2px] text-eggshell`]:
+        isSelected && size === "smallCircle" && colorOption === "highContrast",
 
       // diabled
       "disabled:translate-x-1 disabled:translate-y-1 disabled:border-midnight disabled:bg-midnight disabled:text-jade disabled:hover:cursor-not-allowed disabled:hover:saturate-100":
@@ -99,15 +116,24 @@ const SiteButton: React.FC<ButtonProps> = ({
         size === "extraLarge",
 
       // variant
-      [`border-[2px] border-jade bg-jade ${buttonColors[colorScheme].color7} ${buttonColors[colorScheme].color8}`]:
-        variant === "hollow",
+      [`border-[2px] ${hollowOptions} ${bgOptions} ${buttonColors[colorScheme].color7} ${buttonColors[colorScheme].color8}`]:
+        variant === "hollow" && colorOption === "standard",
+      [`border-[2px] ${hollowOptions} ${bgOptions} ${highContrastButtonColors[colorScheme].color7} ${highContrastButtonColors[colorScheme].color8}`]:
+        variant === "hollow" && colorOption === "highContrast",
       [`${buttonColors[colorScheme].color2} ${buttonColors[colorScheme].color3}`]:
-        variant === "avatar",
-      [`${buttonColors[colorScheme].color2}`]: variant === "filled",
+        variant === "avatar" && colorOption === "standard",
+      [`${highContrastButtonColors[colorScheme].color2} ${highContrastButtonColors[colorScheme].color3}`]:
+        variant === "avatar" && colorOption === "highContrast",
+      [`${buttonColors[colorScheme].color2}`]:
+        variant === "filled" && colorOption === "standard",
+      [`${highContrastButtonColors[colorScheme].color2}`]:
+        variant === "filled" && colorOption === "highContrast",
 
       //selected state
       [` ${buttonColors[colorScheme].color2} ${buttonColors[colorScheme].color4} ${buttonColors[colorScheme].color7} ${buttonColors[colorScheme].color8}`]:
-        isSelected && variant === "hollow",
+        isSelected && variant === "hollow" && colorOption === "standard",
+      [` ${highContrastButtonColors[colorScheme].color2} ${highContrastButtonColors[colorScheme].color4} ${highContrastButtonColors[colorScheme].color7} ${highContrastButtonColors[colorScheme].color8}`]:
+        isSelected && variant === "hollow" && colorOption === "highContrast",
     },
     addClasses,
   );
