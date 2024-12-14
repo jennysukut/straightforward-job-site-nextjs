cd5e4a7f4f0fad8aad4ed0cf15a4a8a9227a71f0
JennySukut <jennysukut@gmail.com>
2024-12-14 14:59:40 -0700

    made jobPostComponent that uses IdealButtonPattern

--- src/app/job-board/page.tsx
+++ src/app/job-board/page.tsx
@@ -1,88 +1,38 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { usePageContext } from "@/contexts/PageContext";
 import { useColorOptions } from "@/lib/stylingData/colorOptions";
 import { useJobListings } from "@/contexts/JobListingsContext";
+import { ButtonColorOption } from "@/lib/stylingData/buttonColors";
 
-import JobListing from "@/components/jobListing";
-import SiteButton from "@/components/siteButton";
-import InfoBox from "@/components/infoBox";
-import Image from "next/image";
-
-import { capitalizeFirstLetter } from "@/utils/textUtils";
+import ShuffleIdealButtonPattern from "@/components/shuffleIdealButtonPattern";
+import JobPost from "@/components/jobPostComponent";
 
 export default function JobBoard() {
   const { accountType } = usePageContext();
   const { textColor } = useColorOptions();
   const { jobListings } = useJobListings();
 
+  const [colorArray, setColorArray] = useState<[]>([]);
+
+  useEffect(() => {
+    ShuffleIdealButtonPattern(setColorArray);
+  }, []);
+
   return (
     <div
       className={`JobBoardPage flex flex-grow flex-col items-center gap-8 md:pb-12 ${textColor}`}
     >
-      Job Board Here
+      <div className="Searchbar">SearchBar Here</div>
       <div className="JobListings flex gap-8">
-        {jobListings?.map((job) => (
-          <div className="JobListing" key={job.jobId}>
-            <InfoBox
-              variant="filled"
-              colorScheme="b1"
-              aria="jobListing"
-              size="profile"
-            >
-              <div className="AppLimitSaveButton -mt-6 flex items-start justify-between pb-8">
-                <div className="AppLimit -ml-4 text-xs font-medium italic">
-                  {job.job?.numberOfApps}/{job.job?.appLimit} apps
-                </div>
-                <button className="SaveButton -mr-4 opacity-100 hover:opacity-80">
-                  <SiteButton
-                    aria="addJobsButton"
-                    size="extraSmallCircle"
-                    variant="filled"
-                    colorScheme="b1"
-                    addClasses="bg-center"
-                    addImage="bg-[url('/save-job-icon.svg')]"
-                  ></SiteButton>
-                </button>
-              </div>
-              <div className="JobDetails flex flex-col gap-1 text-center">
-                <h2 className="JobTitle mb-1">{job.job?.jobTitle}</h2>
-                <p className="BusinessName font-medium italic leading-8">
-                  with {job.job?.businessName}
-                </p>
-                <p className="ExperienceLevel text-sm font-normal">
-                  {capitalizeFirstLetter(job.job?.experienceLevel || "junior")}{" "}
-                  Level
-                </p>
-                <Image
-                  src="/listing-divider.svg"
-                  alt="listingDivider"
-                  width={240}
-                  height={0}
-                  className="my-8"
-                ></Image>
-                {job.job?.locationOption === "remote" && (
-                  <p className="LocationOption">100% Remote</p>
-                )}
-                {job.job?.locationOption === "on-site" && (
-                  <p className="LocationOption">On-Site</p>
-                )}
-                {/* {job.job?.locationOption === "hybrid" && (
-                  <p className="LocationOption">{job.job?.hybridDetails}</p>
-                )} */}
-                <p className="PositionType font-normal italic">
-                  {capitalizeFirstLetter(job.job?.positionType || "")} Position
-                </p>
-                <p className="PayDetails">
-                  {job.job?.payDetails.payscaleMin} -{" "}
-                  {job.job?.payDetails.payscaleMax}
-                </p>
-                <p className="PayOption italic">
-                  {capitalizeFirstLetter(job.job?.payDetails.payOption || "")}
-                </p>
-              </div>
-            </InfoBox>
-          </div>
+        {jobListings?.map((job, jobId) => (
+          <JobPost
+            job={job}
+            index={jobId}
+            colorArray={colorArray}
+            key={jobId}
+          />
         ))}
       </div>
     </div>
---
+++ src/components/jobPostComponent.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Image from "next/image";
+import InfoBox from "./infoBox";
+import SiteButton from "./siteButton";
+
+import { smallShadowColors } from "@/lib/stylingData/smallShadowColors";
+import { useColors } from "@/contexts/ColorContext";
+import { capitalizeFirstLetter } from "@/utils/textUtils";
+import { ButtonColorOption } from "@/lib/stylingData/buttonColors";
+
+import {
+  LargeShadowColorOption,
+  largeShadowColors,
+} from "@/lib/stylingData/largeShadowColors";
+
+interface JobPostProps extends React.HTMLAttributes<HTMLDivElement> {
+  job: any;
+  colorArray: Array<string>;
+  index: any;
+}
+
+const JobPost: React.FC<JobPostProps> = ({ job, colorArray, index }) => {
+  const { colorOption } = useColors();
+
+  const boxOptions =
+    colorOption === "highContrast"
+      ? "border-cobalt drop-shadow-cobalt text-cobalt"
+      : "border-jade drop-shadow-jade text-jade";
+
+  return (
+    <div className="JobListing" key={job.jobId}>
+      <InfoBox
+        variant="filled"
+        colorScheme={colorArray[index % colorArray.length] as ButtonColorOption}
+        aria="jobListing"
+        size="profile"
+      >
+        <div className="AppLimitSaveButton -mt-6 flex items-start justify-between pb-8">
+          <div className="AppLimit -ml-4 text-xs font-medium italic">
+            {job.job?.numberOfApps}/{job.job?.appLimit} apps
+          </div>
+          <button className="SaveButton -mr-4 hover:saturate-150">
+            <SiteButton
+              aria="addJobsButton"
+              size="extraSmallCircle"
+              variant="filled"
+              colorScheme={
+                colorArray[index % colorArray.length] as ButtonColorOption
+              }
+              addClasses="bg-center"
+              addImage="bg-[url('/save-job-icon.svg')]"
+            ></SiteButton>
+          </button>
+        </div>
+        <div className="JobDetails flex flex-col gap-1 text-center">
+          <h2 className="JobTitle mb-1">{job.job?.jobTitle}</h2>
+          <p className="BusinessName font-medium italic leading-8">
+            with {job.job?.businessName}
+          </p>
+          <p className="ExperienceLevel text-sm font-normal">
+            {capitalizeFirstLetter(job.job?.experienceLevel || "junior")} Level
+          </p>
+          {/* divider */}
+          <Image
+            src="/listing-divider.svg"
+            alt="listingDivider"
+            width={240}
+            height={0}
+            className="my-8 opacity-80"
+          ></Image>
+          {job.job?.locationOption === "remote" && (
+            <p className="LocationOption">100% Remote</p>
+          )}
+          {job.job?.locationOption === "on-site" && (
+            <p className="LocationOption">On-Site</p>
+          )}
+          {/* {job.job?.locationOption === "hybrid" && (
+          <p className="LocationOption">{job.job?.hybridDetails}</p>
+        )} */}
+          <p className="PositionType font-normal italic">
+            {capitalizeFirstLetter(job.job?.positionType || "")} Position
+          </p>
+          <p className="PayDetails">
+            {job.job?.payDetails.payscaleMin} -{" "}
+            {job.job?.payDetails.payscaleMax}
+          </p>
+          <p className="PayOption italic">
+            {capitalizeFirstLetter(job.job?.payDetails.payOption || "")}
+          </p>
+        </div>
+      </InfoBox>
+    </div>
+  );
+};
+
+export default JobPost;
