5fcf917dc242b3cce2afd0d3b56e03b78a702764
JennySukut <jennysukut@gmail.com>
2024-12-14 14:39:32 -0700

    created job board listing design

--- src/app/job-board/page.tsx
+++ src/app/job-board/page.tsx
@@ -24,18 +24,17 @@ export default function JobBoard() {
       <div className="JobListings flex gap-8">
         {jobListings?.map((job) => (
           <div className="JobListing" key={job.jobId}>
-            <InfoBox variant="filled" colorScheme="b1" aria="jobListing">
-              <div className="AppLimitSaveButton -mt-8 flex items-start justify-between">
-                <div className="AppLimit -ml-10 mb-4 text-xs font-medium italic">
+            <InfoBox
+              variant="filled"
+              colorScheme="b1"
+              aria="jobListing"
+              size="profile"
+            >
+              <div className="AppLimitSaveButton -mt-6 flex items-start justify-between pb-8">
+                <div className="AppLimit -ml-4 text-xs font-medium italic">
                   {job.job?.numberOfApps}/{job.job?.appLimit} apps
                 </div>
-                <button className="SaveButton -mr-10 opacity-100 hover:opacity-80">
-                  {/* <Image
-                    src="/save-job-icon.svg"
-                    alt="editButton"
-                    width={20}
-                    height={20}
-                  ></Image> */}
+                <button className="SaveButton -mr-4 opacity-100 hover:opacity-80">
                   <SiteButton
                     aria="addJobsButton"
                     size="extraSmallCircle"
@@ -46,14 +45,40 @@ export default function JobBoard() {
                   ></SiteButton>
                 </button>
               </div>
-              <div className="JobDetails text-center">
-                <p className="JobTitle">{job.job?.jobTitle}</p>
-                <p className="BusinessName font-medium italic">
-                  {job.job?.businessName}
+              <div className="JobDetails flex flex-col gap-1 text-center">
+                <h2 className="JobTitle mb-1">{job.job?.jobTitle}</h2>
+                <p className="BusinessName font-medium italic leading-8">
+                  with {job.job?.businessName}
                 </p>
                 <p className="ExperienceLevel text-sm font-normal">
                   {capitalizeFirstLetter(job.job?.experienceLevel || "junior")}{" "}
                   Level
+                </p>
+                <Image
+                  src="/listing-divider.svg"
+                  alt="listingDivider"
+                  width={240}
+                  height={0}
+                  className="my-8"
+                ></Image>
+                {job.job?.locationOption === "remote" && (
+                  <p className="LocationOption">100% Remote</p>
+                )}
+                {job.job?.locationOption === "on-site" && (
+                  <p className="LocationOption">On-Site</p>
+                )}
+                {/* {job.job?.locationOption === "hybrid" && (
+                  <p className="LocationOption">{job.job?.hybridDetails}</p>
+                )} */}
+                <p className="PositionType font-normal italic">
+                  {capitalizeFirstLetter(job.job?.positionType || "")} Position
+                </p>
+                <p className="PayDetails">
+                  {job.job?.payDetails.payscaleMin} -{" "}
+                  {job.job?.payDetails.payscaleMax}
+                </p>
+                <p className="PayOption italic">
+                  {capitalizeFirstLetter(job.job?.payDetails.payOption || "")}
                 </p>
               </div>
             </InfoBox>
