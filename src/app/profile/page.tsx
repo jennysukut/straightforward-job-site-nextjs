c9f1d54b16f4f5f25d09cc3ea5f0531ccc515fba
JennySukut <jennysukut@gmail.com>
2025-03-20 19:12:14 -0600

    limited profile access without logged in status

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,7 +9,7 @@ import { avatarOptions } from "@/lib/stylingData/avatarOptions";
 import { useColors } from "@/contexts/ColorContext";
 import BusinessProfile from "@/components/pages/businessProfile/businessProfile";
 import FellowProfile from "@/components/pages/fellowProfile/fellowProfile";
-
+import { useRouter } from "next/navigation";
 export default function Profile() {
   const {
     setCurrentPage,
@@ -20,27 +20,35 @@ export default function Profile() {
   } = usePageContext();
   const { fellow } = useFellow();
   const { business } = useBusiness();
-
+  const router = useRouter();
   console.log(fellow);
 
   // Set the page type to fellow or business here and render different profiles based on this
   // Once we have signup and login working, we'll be able to grab data on
   // who's logged in and use that to set these details
   useEffect(() => {
-    if (accountType === "Fellow") {
-      setPageType("Fellow");
-      setCurrentPage("fellowProfile");
-    } else if (accountType === "Business") {
-      setPageType("Business");
-      setCurrentPage("businessProfile");
+    if (!isLoggedIn) {
+      router.push("/"); // Redirect to main page
+    } else {
+      if (accountType === "Fellow") {
+        setPageType("Fellow");
+        setCurrentPage("fellowProfile");
+      } else if (accountType === "Business") {
+        setPageType("Business");
+        setCurrentPage("businessProfile");
+      }
     }
   }, [setCurrentPage, setPageType, accountType]);
 
   return (
     <div className="Profile flex flex-grow flex-col items-center gap-8 md:pb-12 md:pt-3">
       {/* here, we'll have to look to make sure the id of the current person is used to set their profile info */}
-      {accountType === "Fellow" && <FellowProfile self={fellow} isOwn />}
-      {accountType === "Business" && <BusinessProfile self={business} isOwn />}
+      {isLoggedIn && accountType === "Fellow" && (
+        <FellowProfile self={fellow} isOwn />
+      )}
+      {isLoggedIn && accountType === "Business" && (
+        <BusinessProfile self={business} isOwn />
+      )}
     </div>
   );
 }
